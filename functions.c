680d91d29cde4622b5f5ff64d51bd6e1d6c7923f
paintproeli <33049894+paintproeli@users.noreply.github.com>
2017-11-10 16:30:41 -0600

    Add files via upload

--- functions.c
+++ functions.c
@@ -6,19 +6,16 @@ void interactive()
 	char *semicolon = NULL;//strtok var
 	int exitstatus = 1;//checks if the user has told the program to exit
 	read = malloc(sizeof(char)*100);
-	int counter = 0;
-	int stdin_cpy;
-	int stdout_cpy;
-	
-	dup2(0,stdin_cpy);
-	dup2(1,stdout_cpy);
+
+
 	while(exitstatus)
 	{
 		printf("prompt> ");
 		fgets(read,100,stdin);//reads commands
 
-		semicolon = strtok(read,"[;]\n");//checks for semicolons within each line read from the file
+		semicolon = strtok(read,"[;|]\n");//checks for semicolons within each line read from the file
 
+	int counter;
 		while (semicolon != NULL)//while there are semicolons in the string left
 		{
 			if(!strcmp(semicolon,"exit"))//if the command is 'exit'
@@ -30,20 +27,17 @@ void interactive()
 			else
 			{
 				execute(semicolon);//give the command and flags to the execute command
-				dup2(stdin_cpy,0);
-				dup2(stdout_cpy,1);
-				counter++;
 			}
 			semicolon = strtok(NULL,"[; ]\n");
 			
 		}
 
-		while(counter)//waits for all children in the line to terminate
+		while(counter)
 		{
 			wait(NULL);
 			counter--;
 		}
-		
+		//wait( ( int *) 0);//waits for child to finish
     
 	}
 	free(semicolon);
@@ -56,11 +50,7 @@ void batch(char *filename)
 	FILE *fstream; // opens a file pointer for handling input from files
 	char *read;//temporary read in variable
 	char *semicolon;//used to store seperate commands
-	int stdin_cpy;
-	int stdout_cpy;
-	
-	dup2(0,stdin_cpy);
-	dup2(1,stdout_cpy);
+
 	read = malloc(sizeof(char)*100);
 
 	fstream = fopen(filename, "r"); // opens the batch file in read mode
@@ -77,8 +67,6 @@ void batch(char *filename)
 		while (semicolon != NULL)//while there are semicolons in the string left
 		{
 			execute(semicolon);//give the command and flags to the execute command
-			dup2(stdin_cpy,0);
-			dup2(stdout_cpy,1);
 			semicolon = strtok(NULL,"[; ]\n");
 		}
 
@@ -90,57 +78,115 @@ void batch(char *filename)
 void execute(char *command)
 {
 
-	char newDirectory[255];
-	getcwd(newDirectory, 255);
 	int counter = 0;//checks the number of args passed to the function
+	int counter2 =0; // runs through the flag array
 	char *flags[100];//
+	char *pipes[100];
 	char *split = NULL;//strtok var
-	char *redirect = NULL;//strtok var
-	char *append = NULL;
-	char *temp = NULL;
+	int last;//holds the last arg position
+	printf("\nExecuting %s \n", command);
 	int fd[2];
 	int stdin_cpy;
 	int stdout_cpy;
-	int last;//holds the last arg position
-	
 	dup2(0,stdin_cpy);
 	dup2(1,stdout_cpy);
-	//strcpy(append,"<<");
-
-	printf("\nExecuting %s \n", command);
+	int pipmode = 0;
+	int pipmode2 = 0;
+	
+	pipe(fd);
 	
 	if(fork() == 0)//creates a child process
 	{
-		
-		if(redirect = strchr(command,'<')) // checks for input operator
+		split = strtok(command, " "); // searches the commands for flags
+		while (split != NULL)
 		{
-			temp = strtok(redirect," <");
-			while (strlen(temp) < 2)
-			{
-				temp = strtok(NULL," <");
-			}
-			fd[0] = open(temp,O_RDONLY);//opens the inputfile
-			//printf("Opened filename %s \n",temp);
-			dup2(fd[0],0);//makes stdin read from the file
-			close(fd[0]);
+			flags[counter]= split;
+			counter++;
+			split = strtok(NULL, " ");//checks for remaining spaces.
+		}
+		last = counter++;
+		flags[last] = NULL; //sets null terminating character
+		counter = 0;
+
 	
-			redirect = strtok(command, "<");
-			//printf("%s\n", redirect);
-			execute(redirect);
-			dup2(stdin_cpy,0);
-			exit(getpid());
-			
-		}
-		else if(redirect = strchr(command,'>')) // checks for output operator
+		while(flags[counter2] != NULL)
 		{
+			pipes[counter2] = flags[counter2];
+			printf("counter2 \n");
+			if( pipmode2 == 1)
+			{
+				pipmode2 = 0;
+				fd[1] = 0;
+				dup2(fd[1],1);//makes stdin read from pipe
+				close(fd[1]);
+//				fd[1] = ;
+				execvp(pipes[0], pipes);
+			}
+			if(!strcmp(flags[counter2], "|"))
+			{
+				pipmode = 1;
+				pipmode2 = 1;
+				pipes[counter2] = NULL;
+				printf("The | op \n");
+				fd[0] = 1;
+				dup2(fd[0],0);//makes stdin read from pipe
+				close(fd[0]);
+//				fd[1] = ;
+				execvp(pipes[0], pipes);	
+				
+			}
+			counter2++;
+				
+		}
+		if( pipmode == 0)
+		{
+			execvp(flags[0], flags);//calls the command
+			printf("EXECVP failed\n");//if the process could not be called, terminate child
+		}
+		exit(getpid());
 		
-		}
-		/*else if (redirect = strchr(command,append)) // searches for append operator
+	}
+	else
+	{
+		wait( ( int *) 0);//waits for child to finish
+	}
+	
+	return;
+}
+
+
+/*
+			pipelining = strtok(semicolon,"[|]\n"); // checks for pipelining
+			if (pipelining != NULL)
+			{
+				while (pipelining != NULL)//while there are semicolons in the string left
+				{
+					if(!strcmp(pipelining,"exit"))//if the command is 'exit'
+					{
+						exitstatus = 0;//tells program to exit after the current line
+					}
+					pipmode = 1;
+					pipecommands[tracker]= pipelining;
+					tracker++;
+					pipelining = strtok(NULL,"[| ]\n");
+				}
+				while(pipecommands[i+1] != NULL)
+				{
+					execute(pipecommands[i], pipmode);//give the command and flags to the execute command
+					i++;
+					pipmode = 0;
+				}
+			}
+*/
+
+/*
+	if ( pipmode == '1')
+	{
+		int fd[2];
+		pipe(fd);
+		if(fork() == 0)//creates a child process
 		{
-			
-		}*/
-		else
-		{
+
 			split = strtok(command, " "); // searches the commands for flags
 			while (split != NULL)
 			{
@@ -149,24 +195,22 @@ void execute(char *command)
 				split = strtok(NULL, " ");//checks for remaining spaces.
 
 			}
-			if(strcmp(flags[0],"cd") == 0)
-			{
-				chdir(flags[1]);
-				return;
-			}
-
 			last = counter++;
 			flags[last] = NULL; //sets null terminating character
+			dup2(fd[1], 1);
+			close(fd[0]);
+			close(fd[1]);
 			execvp(flags[0], flags);//calls the command
+			
 			printf("EXECVP failed\n");//if the process could not be called, terminate child
-			exit(getpid());//terminate the process if it failed
-			free(flags);
-			free(split);
-			free(redirect);
-			//dup2(stdin_cpy,0);
-		
+			exit(getpid());
 		}
-		
-	}
-	return;
-}
+		else
+		{
+			wait( ( int *) 0);//waits for child to finish
+			
+
+		}
+	}else
+	{
+*/
