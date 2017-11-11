eff83a8e2a3a94d9870da08ad6ecda9522efeeeb
DanShadel <32720919+DanShadel@users.noreply.github.com>
2017-11-10 21:42:40 -0600

    part 3 working

--- functions.c
+++ functions.c
@@ -37,7 +37,6 @@ void interactive()
 			semicolon = strtok(NULL,"[; ]\n");
 			
 		}
-
 		while(counter)//waits for all children in the line to terminate
 		{
 			wait(NULL);
@@ -98,20 +97,69 @@ void execute(char *command)
 	char *redirect = NULL;//strtok var
 	char *append = NULL;
 	char *temp = NULL;
+	char *temp2 = NULL;
+	char *pipeline = NULL;
+	char *cmdcpy;
 	int fd[2];
+	int i;
+	int cmdctr = 0;
+	int pipefd1[2];
+	int pipefd2[2];
 	int stdin_cpy;
 	int stdout_cpy;
 	int last;//holds the last arg position
+	int check1 = 0;
 	
-	dup2(0,stdin_cpy);
-	dup2(1,stdout_cpy);
-	//strcpy(append,"<<");
-
-	printf("\nExecuting %s \n", command);
+	cmdcpy = malloc(sizeof(char) * strlen(command));
+	
+	strcpy(cmdcpy, command);
+	
 	
 	if(fork() == 0)//creates a child process
 	{
+		/*if(temp = strchr(command, '|'))
+		{
+	
 		
+			pipeline = strtok(command, "[|]");
+			
+			while(pipeline != NULL)
+			{
+				cmdctr++;
+				pipeline = strtok(NULL , "|");
+			}
+			
+			pipe(pipefd1);
+			pipe(pipefd2);
+			
+			pipeline = strtok(command, "[|]");
+			printf( "%d %s\n", cmdctr, pipeline);
+			
+			dup2(pipefd1[1],1);
+			execute(pipeline);
+			wait(NULL);
+			dup2(pipefd1[0],0);
+	
+			for(i = 0; i < cmdctr - 1;i++)
+			{
+				pipeline = strtok(NULL, "[|]");
+				dup2(pipefd1[0],0);
+				dup2(pipefd2[1],1);
+				execute(pipeline);
+				dup2(pipefd2[0],0);
+				dup2(pipefd1[0],0);
+				wait(NULL);
+					
+			}
+			pipeline = strtok(NULL, "[|]");
+			dup2(pipefd2[0],0);
+			dup2(stdout_cpy,1);
+			execute(pipeline);
+	
+			exit(getpid());
+		}
+		*/
+
 		if(redirect = strchr(command,'<')) // checks for input operator
 		{
 			temp = strtok(redirect," <");
@@ -120,53 +168,75 @@ void execute(char *command)
 				temp = strtok(NULL," <");
 			}
 			fd[0] = open(temp,O_RDONLY);//opens the inputfile
-			//printf("Opened filename %s \n",temp);
+			//printf("Opened filename %s \n",temp);dup2(fd[0],0);//makes stdin read from the file
 			dup2(fd[0],0);//makes stdin read from the file
 			close(fd[0]);
-	
 			redirect = strtok(command, "<");
-			//printf("%s\n", redirect);
-			execute(redirect);
-			dup2(stdin_cpy,0);
-			exit(getpid());
-			
+			check1=1;
+	
 		}
-		else if(redirect = strchr(command,'>')) // checks for output operator
-		{
+	
 		
-		}
-		/*else if (redirect = strchr(command,append)) // searches for append operator
+		if (redirect = strstr(cmdcpy,">>")) // searches for append operator
 		{
-			
-		}*/
-		else
-		{
-			split = strtok(command, " "); // searches the commands for flags
-			while (split != NULL)
+			temp = strtok(redirect, "> ");
+			while(strlen(temp) < 2)
 			{
-				flags[counter]= split;
-				counter++;
-				split = strtok(NULL, " ");//checks for remaining spaces.
-
+					temp = strtok(NULL, ">");
 			}
-			if(strcmp(flags[0],"cd") == 0)
+			fd[1] = open(temp, O_WRONLY | O_APPEND | O_CREAT, S_IRUSR | S_IWUSR); //opens and sets append
+			dup2(fd[1], 1);
+			close(fd[1]);
+
+		}
+		else if(redirect = strchr(cmdcpy,'>')) // checks for output operator
+		{
+			
+			
+			temp = strtok(redirect," >"); //tokenizes up to the output operator
+			while (strlen(temp) < 2)
 			{
-				chdir(flags[1]);
-				return;
+				temp = strtok(NULL," >"); //
 			}
-
-			last = counter++;
-			flags[last] = NULL; //sets null terminating character
-			execvp(flags[0], flags);//calls the command
-			printf("EXECVP failed\n");//if the process could not be called, terminate child
-			exit(getpid());//terminate the process if it failed
-			free(flags);
-			free(split);
-			free(redirect);
-			//dup2(stdin_cpy,0);
+			fd[1] = open(temp, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);//opens the outputfile, ro create a new one
+			//printf("Opened filename %s \n",temp);
+			dup2(fd[1],1);//makes stdout write from the file
+			close(fd[1]); //closes fd after use
+			
 		
 		}
 		
+		if(check1 != 1)
+		{
+			redirect = strtok(command, ">");
+		}
+
+		split = strtok(command, " "); // searches the commands for flags
+		while (split != NULL)
+		{
+			
+			flags[counter]= split;
+			counter++;
+			split = strtok(NULL, " ");//checks for remaining spaces.
+		}
+		if(strcmp(flags[0],"cd") == 0)
+		{
+			chdir(flags[1]);
+			return;
+		}
+		last = counter++;
+		flags[last] = NULL; //sets null terminating character
+		execvp(flags[0], flags);//calls the command
+		printf("EXECVP failed\n");//if the process could not be called, terminate child
+		exit(getpid());//terminate the process if it failed
+		free(flags);
+		free(split);
+		free(redirect);
+		dup2(stdout_cpy, 1);
+		dup2(stdin_cpy,0);
+			
+
+
 	}
 	return;
 }
